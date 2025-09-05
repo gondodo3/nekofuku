a733640fb00227fb7ffd3ef03b1fd647677e7071
gondodo3 <yuugon2019@icloud.com>
2025-09-06 05:37:59 +0900

    エラーを消しました

--- src/quizzes/quizdata.ts
+++
@@ -1,9 +0,0 @@
-// クイズ型定義
-export type Quiz = {
-	id: number;
-	question: string;
-	choices: string[];
-	answer: number; // 正解の選択肢インデックス
-	explanation: string;
-	correctMark: string; // "○" か "×"
-};
